f9ee13819a2cfb29f30c8eb7b4918ee545e936a4
David7ce <79609649+David7ce@users.noreply.github.com>
2024-07-06 12:03:14 +0100

    Update arksys.sh

--- script/arksys.sh
+++ script/arksys.sh
@@ -1,100 +1,98 @@
 #!/bin/bash
-# WARNING: this script will destroy data on the selected disk.
-# This script can be run by executing the following:
-# curl -sL https://raw.githubusercontent.com/arksys-os/arksys_basic-install/main/installation/script/arksys.sh | bash
-## curl -sL https://dub.sh/MpD1lZh | bash
 
 echo "arksys installation"
 
-# 0. Set the console keyboard layout
+# Prompt for device to use for installation
+read -rp "Enter the device to install Arch Linux (e.g., /dev/nvme0n1 or /dev/sda): " device
+
+# 0. Set the console keyboard layout (optional)
 # loadkeys es
 
 # 1.1. Update the system clock
-timedatectl status
+timedatectl set-ntp true
 
-# ------------------ #
-
-# 1.2. Setup the disk and partitions, /dev/nvme0n1 or /dev/sda1
+# 1.2. Partition the disk
 swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
-swap_end=$(( $swap_size + 129 + 1 ))MiB
+swap_end=$(( swap_size + 129 + 1 ))MiB
 
-parted --script "${device}" -- mklabel gpt \
-  mkpart ESP fat32 1Mib 129MiB \
-  set 1 boot on \
-  mkpart primary linux-swap 129MiB ${swap_end} \
-  mkpart primary ext4 ${swap_end} 100%
+parted --script "${device}" \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 129MiB \
+    set 1 boot on \
+    mkpart primary linux-swap 129MiB ${swap_end} \
+    mkpart primary ext4 ${swap_end} 100%
 
-# Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
-# but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
-part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
-part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
-part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
+# Identify partitions
+part_boot="$(ls "${device}"* | grep -E "^${device}p?1$")"
+part_swap="$(ls "${device}"* | grep -E "^${device}p?2$")"
+part_root="$(ls "${device}"* | grep -E "^${device}p?3$")"
 
+# Wipe filesystem signatures
 wipefs "${part_boot}"
 wipefs "${part_swap}"
 wipefs "${part_root}"
 
-# 1.3. Then format the partitions
-mkfs.vfat -F32 "${part_boot}" # EFI partition formatted to fat32
+# 1.3. Format the partitions
+mkfs.vfat -F32 "${part_boot}"
 mkswap "${part_swap}"
 mkfs.ext4 "${part_root}"
 
-# 1.4. Mount the file systems (dev/nvme0nx or /dev/sdax)
+# 1.4. Mount the file systems
 swapon "${part_swap}"
 mount "${part_root}" /mnt
-mount --mkdir "${part_boot}" /mnt/boot
-# ------------------ #
+mkdir -p /mnt/boot
+mount "${part_boot}" /mnt/boot
 
 # 2. Install base system
-pacman -Sy
-pacstrap -K /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware
 
 # 3. Configure the system
-# 3.1. Fstab and chroot
+# 3.1. Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
-# 3.2. Timezone and localization
+# 3.2. Chroot into the new system
+arch-chroot /mnt /bin/bash <<EOF
+# Set timezone
 ln -sf /usr/share/zoneinfo/Atlantic/Canary /etc/localtime
 hwclock --systohc
 
+# Generate locales
 locale-gen
-'LANG=en-US.UTF-8' >> /etc/locale.conf  # echo "LANG=en_GB.UTF-8" > /mnt/etc/locale.conf
-'arksys' >> /etc/hostname
-'KEYMAP=es' >> /etc/vconsole.conf
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 
-# 3.3. Network configuration
-# 3.3.1. Hostname
-'ark' >> /etc/hostname
-# 3.3.2. Hosts
-'127.0.0.1 localhost\n ::1 localhost\n 127.0.0.1 arksys.localdomain hostname' >> /etc/hosts
+# Set hostname
+echo 'arksys' > /etc/hostname
 
-# 3.3.3. Initramfs  mkinitcpio -P
+# Set console keymap
+echo 'KEYMAP=es' > /etc/vconsole.conf
 
-# 3.4. Set user
-$user = ark
-passwd
-useradd -m ark
-passwd ark 
-usermod -aG wheel,audio,video,optical,storage ark
-pacman -S sudo
-# EDITOR=nvim visudo # Delete comment in line: %wheel ALL=(ALL:ALL) ALL
+# 3.3. Network configuration (optional)
+# Set network hostname
+echo '127.0.0.1 localhost' > /etc/hosts
+echo '::1 localhost' >> /etc/hosts
+echo '127.0.1.1 arksys.localdomain arksys' >> /etc/hosts
 
-# 3.5. Boot-loader
+# 3.4. Set root password (change '123' to your desired password)
+echo '123' | passwd --stdin root
+
+# 3.5. Create a new user (change 'ark' and 'arkpass' to your desired username and password)
+useradd -m -G wheel,audio,video,optical,storage ark
+echo 'arkpass' | passwd --stdin ark
+
+# Allow members of wheel group to execute any command with sudo
+sed -i '/%wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
+
+# 3.6. Install and configure bootloader (assuming UEFI system)
 pacman -S grub efibootmgr dosfstools os-prober mtools
-mkdir /boot/EFI
-mount /dev/sda1 /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+mkdir -p /boot/EFI
+mount "${part_boot}" /boot/EFI
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=grub_uefi --recheck
 
-# After installing bootloader change grub config to: GRUB_TIMEOUT=0
-nvim /etc/default/grub # Make changes
+# Generate grub configuration
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# 4. Reboot
-exit
-umount -l /mnt
-reboot
+EOF
 
-# 5. Post-installation (install Desktop Environment)
-sudo pacman -S plasma-desktop 
-systemctl enable NetworkManager # for GNOME and KDE
+# 4. Unmount and reboot
+umount -R /mnt
+reboot
